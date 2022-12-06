ec2f980eafb7fca2bf115316f84c7e582f30d4c1
DESKTOP-1LH23A7\268ga <rap.97268@gmail.com>
2022-12-07 00:35:55 +0900

    スタイルの調整前

--- js/script.js
+++ js/script.js
@@ -1,39 +1,84 @@
-const characters = document.querySelectorAll(".character");
-const container = document.querySelector("#container");
+// canvas要素の取得
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
 
-// キャラクターの初期位置と移動方向を設定する
-const characterPositions = [];
-const characterDirections = [];
-for (let i = 0; i < characters.length; i++) {
-  characterPositions.push({
-    x: Math.random() * container.offsetWidth,
-    y: Math.random() * container.offsetHeight,
-  });
-  characterDirections.push({
-    x: Math.random() * 2 - 1,
-    y: Math.random() * 2 - 1,
-  });
+// 文字を描画するための配列
+const chars = ['パ', 'イ', 'ケ', 'ツ', 'ポ', 'ロ', 'リ'];
+
+// 文字の情報を格納するためのオブジェクト
+const charData = {};
+
+// 文字の数
+const charNum = 7;
+
+// 文字の大きさ
+const charSize = 20;
+
+// 初期化処理
+function init() {
+    // canvasの大きさを設定
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+
+    // 文字をランダムに配置する
+    for (let i = 0; i < charNum; i++) {
+        // 文字の種類をランダムに決定
+        //const char = chars[Math.floor(Math.random() * chars.length)];
+        // 文字の種類を配列から取り出す
+        const char = chars[i % chars.length];
+
+        // 文字の初期位置をランダムに決定
+        const x = Math.random() * (canvas.width - charSize);
+        const y = Math.random() * (canvas.height - charSize);
+
+        // 文字の初期速度をランダムに決定
+        const vx = Math.random() * 2 - 1;
+        const vy = Math.random() * 2 - 1;
+
+        // 文字の情報を格納
+        charData[i] = { char, x, y, vx, vy };
+    }
 }
 
-// 1フレームごとにキャラクターを移動させる
-function updateCharacters() {
-  for (let i = 0; i < characters.length; i++) {
-    // キャラクターを移動させる
-    characterPositions[i].x += characterDirections[i].x;
-    characterPositions[i].y += characterDirections[i].y;
+// 描画処理
+function draw() {
+    // canvasをクリアする
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-    // 画面の端に達した場合は方向を反転させる
-    if (characterPositions[i].x < 0 || characterPositions[i].x > container.offsetWidth) {
-      characterDirections[i].x *= -1;
-    }
-    if (characterPositions[i].y < 0 || characterPositions[i].y > container.offsetHeight) {
-      characterDirections[i].y *= -1;
-    }
+    // 文字の情報を取得
+    for (const key in charData) {
+        const { char, x, y, vx, vy } = charData[key];
 
-    // キャラクターを移動させた位置に配置する
-    characters[i].style.left = characterPositions[i].x + "px";
-    characters[i].style.top = characterPositions[i].y + "px";
+        // 文字を描画
+        ctx.font = `${charSize}px sans-serif`;
+        ctx.fillText(char, x, y);
+
+        // 文字の位置を更新
+        charData[key].x += vx;
+        charData[key].y += vy;
+
+        // 文字が画面外に出た場合の処理
+        if (charData[key].x < 0 || charData[key].x > canvas.width - charSize) {
+            charData[key].vx *= -1;
+        }
+        if (charData[key].y < 0 || charData[key].y > canvas.height - charSize) {
+            charData[key].vy *= -1;
+        }
+    }
+}
+
+// ループ処理
+function loop() {
+    // 描画処理
+    draw();
+  
+    // 一定時間後に再度ループ処理を実行
+    setTimeout(loop, 10);
   }
-}
-
-setInterval(updateCharacters, 1000 / 60); // 60fpsで更新する
+  
+  // 初期化処理を実行
+  init();
+  
+  // ループ処理を実行
+  loop();
+  
